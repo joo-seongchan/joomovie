e7f6cb37dacb5ec30633b059a4e24541cca4a39b
joo-seongchan <jskjsj601@gmail.com>
2022-06-24 12:55:24 +0900

    api

---
+++ src/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "32dc4b3bc05bac57e70db66b64d774c6",
+    language: "ko-kr",
+  },
+});
+
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  topRated: () => api.get("movie/top_rated"),
+  latest: () => api.get("movie/latest"),
+  upComming: () => api.get("movie/upcoming"),
+};
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,3 +1,20 @@
+import { useEffect } from "react";
+import { movieApi } from "../../../api";
+// console.log(movieApi.nowPlaying());
+// console.log(movieApi.nowPlaying());
+
 export const Home = () => {
+  useEffect(() => {
+    const movieData = async () => {
+      const {
+        data: { results },
+      } = await movieApi.nowPlaying();
+      console.log(results);
+      // console.log(playing);
+      // console.log(await movieApi.nowPlaying());
+    };
+    movieData();
+  }, []);
+
   return <div>Home</div>;
 };
