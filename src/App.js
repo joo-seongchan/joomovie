d80b5d07eae5bb7a26819b983f686225ae2c2679
joo-seongchan <jskjsj601@gmail.com>
2022-06-24 11:05:12 +0900

    router&create components & helmet &global styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import { HelmetProvider } from "react-helmet-async";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { Detail } from "./components/pages/detail/Detail";
+import { Home } from "./components/pages/home/Home";
+import { NotFound } from "./components/pages/NotFound";
+import { Search } from "./components/pages/search/Search";
+import { GlobalStyle } from "./styles/globalStyle";
+
 function App() {
-  return <div>Start!</div>;
+  return (
+    <HelmetProvider>
+      <Router>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/detail/:id" element={<Detail />}></Route>
+          <Route path="/search" element={<Search />}></Route>
+          <Route path="/*" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
+    </HelmetProvider>
+  );
 }
 
 export default App;
---
+++ src/styles/globalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+export const GlobalStyle = createGlobalStyle`
+${reset}
+*{
+    box-sizing: border-box;
+}
+body{
+    font-family: 'Noto Sans KR', sans-serif;
+}
+
+a{
+    text-decoration: none;
+    color: white;
+}
+`;
